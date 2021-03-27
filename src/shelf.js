901a8275fa9bb7ad8e894493edbf85f4b2c972ad
Shauna Myers <shaunamyers26@gmail.com>
2021-03-26 21:33:24 -0400

    Udate and complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+};
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+    shelf.splice([i],1);
+    };
+  }
+};
+
+function listTitles(shelf) {
+  var list = [];
+  for (var i = 0; i < shelf.length; i++) {
+    list.push(shelf[i].title);
+  }
+  return list.join(", ");
+}
+
+function searchShelf(shelf, title) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+    }
+    return titles.includes(title);
+  }
+
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
