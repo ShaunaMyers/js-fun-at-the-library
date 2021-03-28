9c419753689aca23e70689a2c776054ba1dc3bc0
Shauna Myers <shaunamyers26@gmail.com>
2021-03-28 19:16:12 -0400

    Update library.js file for project specific changes

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,73 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+   }
+   return library
+  }
+
+//REfACTOR
+function addBook(name, book) {
+  if (book.genre === "fantasy") {
+    name.shelves.fantasy.push(book);
+  } else if (book.genre === "nonFiction") {
+    name.shelves.nonFiction.push(book);
+  } else {
+    name.shelves.fiction.push(book);
+  }
+}
+
+
+// function checkoutBook(library, title, genre) {
+//   if (title === library.shelves[genre][0].title) {
+//     library.shelves[genre].pop();
+//     return `You have now checked out ${title} from the ${library.name}`;
+//   } else if (title !== library.shelves[genre][0].title){
+//     return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+//   }
+// }
+
+function checkoutBook(library, title, genre) {
+  if (title === library.shelves[genre][0].title) {
+    library.shelves[genre].pop();
+    return `You have now checked out ${title} from the ${library.name}`;
+  }
+}
+
+
+// function checkoutBook(library, title, genre) {
+//   var contents = [];
+//   if (title === library.shelves[genre][0].title) {
+//     library.shelves[genre].pop();
+//     return `You have now checked out ${title} from the ${library.name}`;
+//   } else {
+//       return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+//   }
+// }
+
+
+function checkoutBook(library, title, genre) {
+  var contents = [];
+  if (title === library.shelves[genre][0].title) {
+    library.shelves[genre].pop();
+    return `You have now checked out ${title} from the ${library.name}`;
+  } else {
+      return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  }
+}
+
+
+
+
+//HINT: each test is independent. Imagine you're wiping slate blank
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
